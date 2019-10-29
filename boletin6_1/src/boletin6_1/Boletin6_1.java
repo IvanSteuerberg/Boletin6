43d88c2e7d50e592d74080848762cceba2ac2625
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-29 11:19:09 +0100

    Ejercicio 1 boletin 6

---
+++ boletin6_1/src/boletin6_1/Boletin6_1.java
@@ -0,0 +1,22 @@
+package boletin6_1;
+// @author irodriguezsteuerberg
+
+import java.util.Scanner;
+
+ 
+public class Boletin6_1 {
+  
+    public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Introduce un numero");
+int numero=sc.nextInt();
+Metodos obx1 = new Metodos();
+obx1.comparar(numero);
+
+
+
+
+
+    }
+    
+}
---
+++ boletin6_1/src/boletin6_1/Metodos.java
@@ -0,0 +1,22 @@
+package boletin6_1;
+//@author irodriguezsteuerberg
+ 
+public class Metodos {
+private int numero;
+
+public Metodos(){    
+}    
+public Metodos(int numero){
+this.numero=numero;    
+}
+public void comparar(int numero){
+if (numero>=0)
+System.out.println("É positivo");     
+   
+
+}
+
+
+
+
+}
